306bcaeb71aab5c7e834923ef0b68b130b8c17aa
Kush Patel <kushpatel1928@gmail.com>
2024-01-02 20:46:54 -0500

    all routes to all tables created

--- server/server.js
+++ server/server.js
@@ -8,16 +8,7 @@ const app = express(); // create express app
 app.use(express.json()); 
 app.use(cors());
 
-/*
 // connect to mongodb
-mongoose.connect('mongodb+srv://kushpatel:haTZRTXVWw53OtU3@fantasymusic.ammhffo.mongodb.net/?retryWrites=true&w=majority', { 
-    // useNewUrlParser: true, 
-    // useUnifiedTopology: true  // these are not required because they are now default
-})
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
-*/
-
 const uri = "mongodb+srv://kushpatel:haTZRTXVWw53OtU3@fantasymusic.ammhffo.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,37 +18,49 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+// Connect the client to the server	(optional starting in v4.7)
+client.connect();
 
-    // routes
-    const overallDB = client.db("fantasy_music").collection("overall"); // overall collection of the database
+// Send a ping to confirm a successful connection
+client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    // landing page
-    app.get('/', async (req, res) => {
-        res.send('Landing Page');
-    });
+// routes
+const overallDB = client.db("fantasy_music").collection("overall"); // overall collection of the database
+const billboardDB = client.db("fantasy_music").collection("billboard_hot_100_artists"); // billboard collection of the database
+const billboardAlbumsDB = client.db("fantasy_music").collection("billboard_hot_200_albums"); // billboard collection of the database 
+const billboardSongsDB = client.db("fantasy_music").collection("billboard_hot_200_songs"); // billboard collection of the database   
 
-    // get overall data
-    app.get('/overall', async (req, res) => {
-        const overallData = await overallDB.find({}).toArray(); // find all data in the overall collection of the database
-        res.json(overallData);
-    });
+// landing page
+app.get('/', async (req, res) => {
+    res.send('Landing Page');
+});
 
-    app.listen(3001, () => {
-        console.log('Server listening on port 3001');
-    });
+// get overall data
+app.get('/overall', async (req, res) => {
+    const overallData = await overallDB.find({}).toArray(); // find all data in the overall collection of the database
+    res.json(overallData);
+});
 
-  } 
-  catch (err) {
-    console.log(err.stack);
-  }
-}
-run().catch(console.dir); // this calls the async function and the 
+// get billboard hot 100 artists data
+app.get('/billboard-hot-100-artists', async (req, res) => {
+    const billboardData = await billboardDB.find({}).toArray(); // find all data in the overall collection of the database
+    res.json(billboardData);
+});
 
+// get billboard hot 200 albums data
+app.get('/billboard-hot-200-albums', async (req, res) => {
+    const billboardAlbumsData = await billboardAlbumsDB.find({}).toArray(); // find all data in the overall collection of the database
+    res.json(billboardAlbumsData);
+});
+
+// get billboard hot 200 songs data
+app.get('/billboard-hot-200-songs', async (req, res) => {
+    const billboardSongsData = await billboardSongsDB.find({}).toArray(); // find all data in the overall collection of the database
+    res.json(billboardSongsData);
+});
+
+app.listen(3001, () => {
+    console.log('Server listening on port 3001');
+});
