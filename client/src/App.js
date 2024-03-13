b100ddcaa4d31fa827bbf8d2d7418d73b1731afc
Kush Patel <kushpatel1928@gmail.com>
2024-03-12 23:18:55 -0400

    added buttons to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.png';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
         <p className="App-title">
           fantasy music
         </p>
+        <div className="Button-Pages">
+        <Button className="button1" variant="contained">Overall Standings</Button>
+        <Button className="button1" variant="contained">Billboard 100 Songs</Button>
+        <Button className="button1" variant="contained">Billboard 100 Artists</Button>
+        <Button className="button1" variant="contained">Billboard 200 Albums</Button>
+        </div>
       </header>
     </div>
   );
