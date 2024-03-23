61d1c5734760862dcd5460bb1ffd09c3e9caeaf3
Kush Patel <kushpatel1928@gmail.com>
2024-03-22 23:00:44 -0400

    attempts to create routes for App.js

---
+++ client/src/overall.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OverallStandings() {
+    return (
+          <div>
+            <p>Overall Standings hiii</p>
+          </div>
+    );
+}
+
+export default OverallStandings;
