a07c995f176096a68b6a9d3567818ce7398e71e4
Kush Patel <kushpatel1928@gmail.com>
2024-04-24 19:50:54 -0400

    added mui table for overall standings

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.png';
 import './App.css';
 import Button from '@mui/material/Button';
+import OverallStanding from './components/overallStanding';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-title">
-            fantasy music
-          </p>
-        <div className="Button-Pages">
-          <Button variant='outlined' to='/overall'>Overall Standings</Button>
-          <Button variant='outlined'>Billboard 100 Songs</Button>
-          <Button variant='outlined'>Billboard 100 Artists</Button>
-          <Button variant='outlined'>Billboard 200 Albums</Button>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+                <p className="App-title">
+                  fantasy music
+                </p>
+              <div className="Button-Pages">
+                <Link to="/overall"><Button variant='outlined'>Overall Standings</Button></Link>
+                <Button variant='outlined'>Billboard 100 Songs</Button>
+                <Button variant='outlined'>Billboard 100 Artists</Button>
+                <Button variant='outlined'>Billboard 200 Albums</Button>
+              </div>
+            </header>
+            </>
+          } />
+          <Route path="/overall" element={<OverallStanding />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/overallStanding.js
@@ -0,0 +1,157 @@
+import React from "react";
+import './Standings.css';
+
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+const columns = [
+  { id: 'artist', label: 'Artist', minWidth: 170 },
+  { id: 'name', label: 'Name', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+export default function OverallStanding() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="Standings">
+        <header className="Standings-header">
+        <p className="Standings-title">
+            overall standings
+        </p>
+        </header>
+        <Box sx={{ width: '90%', typography: 'body1', margin: 'auto' }}>
+        <TabContext value={value}>
+            <Box sx={{borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab label="Standings" value="1" />
+                <Tab label="My Team" value="2" />
+            </TabList>
+            </Box>
+            <TabPanel value="1">
+                <Paper sx={{ width: '100%', overflow: 'hidden', margin: 'auto' }}>
+                    <TableContainer sx={{ maxHeight: 550 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                key={column.id}
+                                align={column.align}
+                                style={{ minWidth: column.minWidth }}
+                                >
+                                {column.label}
+                                </TableCell>
+                            ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                    {columns.map((column) => {
+                                    const value = row[column.id];
+                                    return (
+                                        <TableCell key={column.id} align={column.align}>
+                                        {column.format && typeof value === 'number'
+                                            ? column.format(value)
+                                            : value}
+                                        </TableCell>
+                                    );
+                                    })}
+                                </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[10, 25, 100]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Paper>
+            </TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+        </TabContext>
+        </Box>
+    </div>
+  );
+}
