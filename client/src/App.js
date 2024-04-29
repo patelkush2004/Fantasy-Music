df4eb0d858a498c545cc3251fb157ce14d707a69
Kush Patel <kushpatel1928@gmail.com>
2024-04-28 23:06:56 -0400

    added data for all tables

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import logo from './logo.png';
 import './App.css';
 import Button from '@mui/material/Button';
 import OverallStanding from './components/overallStanding';
+import BillboardArtists from './components/billboardArtists';
+import BillboardSongs from './components/billboardSongs';
+import BillboardAlbums from './components/billboardAlbums';
 
 function App() {
   return (
@@ -19,14 +22,17 @@ function App() {
                 </p>
               <div className="Button-Pages">
                 <Link to="/overall"><Button variant='outlined'>Overall Standings</Button></Link>
-                <Button variant='outlined'>Billboard 100 Songs</Button>
-                <Button variant='outlined'>Billboard 100 Artists</Button>
-                <Button variant='outlined'>Billboard 200 Albums</Button>
+                <Link to="/billboard-artists"><Button variant='outlined'>Billboard 100 Artists</Button></Link>
+                <Link to="/billboard-songs"><Button variant='outlined'>Billboard 200 Songs</Button></Link>
+                <Link to="/billboard-albums"><Button variant='outlined'>Billboard 200 Albums</Button></Link>
               </div>
             </header>
             </>
           } />
           <Route path="/overall" element={<OverallStanding />} />
+          <Route path="/billboard-artists" element={<BillboardArtists />} />
+          <Route path="/billboard-songs" element={<BillboardSongs />} />
+          <Route path="/billboard-albums" element={<BillboardAlbums />} />
         </Routes>
       </div>
     </Router>
---
+++ client/src/components/billboardAlbums.js
@@ -0,0 +1,136 @@
+import React from "react";
+import './Standings.css';
+import { useState, useEffect } from 'react';
+
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+const OVERALL = "http://localhost:3001/billboard-hot-200-albums";
+
+const columns = [
+  { id: 'rank', label: 'Rank', minWidth: 50, align: 'left' },
+  { id: 'album_cover', label: 'Album Cover', minWidth: 50, align: 'center'},
+  { id: 'artist', label: 'Name', minWidth: 310 },
+  {
+    id: 'album',
+    label: 'Album',
+    minWidth: 310,
+    align: 'left',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+];
+
+export default function BillboardAlbums() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState([]);
+
+  const getRows = async () => {
+    const response = await fetch(OVERALL);
+    const data = await response.json();
+    setRows(data);
+  }
+
+  useEffect(() => {
+    getRows();
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="Standings">
+        <p className="Standings-title">
+            billboard hot 200 albums
+        </p>
+        <Box sx={{ width: '90%', typography: 'body1', margin: 'auto' }}>
+        <TabContext value={value}>
+            <Box sx={{borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab label="Standings" value="1" />
+                <Tab label="My Team" value="2" />
+            </TabList>
+            </Box>
+            <TabPanel value="1">
+                <Paper sx={{ width: '100%', overflow: 'hidden', margin: 'auto' }}>
+                    <TableContainer sx={{ maxHeight: 680 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                key={column.id}
+                                align={column.align}
+                                style={{ minWidth: column.minWidth }}
+                                sx={{ fontWeight: 'bold', backgroundColor: '#48A2EE', color: 'white'}}
+                                >
+                                {column.label}
+                                </TableCell>
+                            ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code} sx={{ height: '20px' }}>
+                                    {columns.map((column) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                            {column.id === 'album_cover' ? 
+                                                <img src={value} alt="album_cover" style={{width: '66px', height: '66px', borderRadius: '50%'}}/> :
+                                                (column.format && typeof value === 'number' ? column.format(value) : value)
+                                            } 
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[10, 25, 100]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                        sx={{ fontWeight: 'bold', backgroundColor: '#48A2EE', color: 'white'}}
+                    />
+                </Paper>
+            </TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+        </TabContext>
+        </Box>
+    </div>
+  );
+}
